d7d9960b47783ee72df274ed32d80d26b6fd294a
tilkov03 <tilkovpetko40@gmail.com>
2024-11-10 23:05:51 +0200

    Delete FreelancePlatform directory

--- FreelancePlatform/src/main/java/com/freelance/app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.freelance.app.repository;
-
-import com.freelance.app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
